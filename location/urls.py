f34f9a9de25d0db834d347fca0d6595190df5951
gilberto trindade <g.trindade@gmail.com>
2025-03-24 15:23:19 -0300

    url

--- location/urls.py
+++ location/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('track/', views.track, name='track'),
-    path('logs/', views.view_logs, name='view_logs'),
+    path('compra_venda/', views.track, name='track'),
+    path('dog/', views.view_logs, name='view_logs'),
 ]
